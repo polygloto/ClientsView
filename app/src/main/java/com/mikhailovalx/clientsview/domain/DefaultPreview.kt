1a346a301a2b42c09134f2abca7699ef7e01e203
Mikhailov <aleksandr.mihajlov@krit.pro>
2021-10-03 21:42:20 +0300

    update dependencies

---
+++ app/src/main/java/com/mikhailovalx/clientsview/domain/DefaultPreview.kt
@@ -0,0 +1,127 @@
+package com.mikhailovalx.clientsview.domain
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import be.sigmadelta.calpose.Calpose
+import be.sigmadelta.calpose.WEIGHT_7DAY_WEEK
+import be.sigmadelta.calpose.model.CalposeActions
+import be.sigmadelta.calpose.model.CalposeWidgets
+import be.sigmadelta.calpose.widgets.DefaultDay
+import be.sigmadelta.calpose.widgets.DefaultHeader
+import com.mikhailovalx.clientsview.core.theme.TurquoiseColor
+import org.threeten.bp.YearMonth
+
+@SuppressLint("NewApi")
+@Preview("DefaultPreview")
+@Composable
+fun DefaultPreview() {
+    var month by remember { mutableStateOf(YearMonth.now()) }
+    DefaultCalendar(
+        month = month,
+        actions = CalposeActions(
+            onClickedPreviousMonth = { month = month.minusMonths(1) },
+            onClickedNextMonth = { month = month.plusMonths(1) }
+        )
+    )
+}
+
+@SuppressLint("NewApi")
+@Composable
+fun DefaultCalendar(
+    month: YearMonth,
+    actions: CalposeActions
+) {
+    Calpose(
+        month = month,
+        actions = actions,
+        widgets = CalposeWidgets(
+            header = { month, todayMonth, actions ->
+                DefaultHeader(month, todayMonth, actions)
+            },
+            headerDayRow = { headerDayList ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(1f)
+                        .padding(bottom = 16.dp),
+                ) {
+                    headerDayList.forEach {
+                        DefaultDay(
+                            text = it.name.first().toString(),
+                            modifier = Modifier.weight(WEIGHT_7DAY_WEEK),
+                            style = TextStyle(color = Color.Gray)
+                        )
+                    }
+                }
+            },
+            day = { dayDate, todayDate ->
+                val isToday = dayDate == todayDate
+                val dayHasPassed = dayDate.day < todayDate.day
+                val isCurrentMonth = dayDate.month == todayDate.month
+
+                val widget: @Composable () -> Unit = {
+                    val weight = if (isToday) 1f else WEIGHT_7DAY_WEEK
+                    DefaultDay(
+                        text = dayDate.day.toString(),
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .weight(weight)
+                            .fillMaxWidth(),
+                        style = TextStyle(
+                            color = when {
+                                isCurrentMonth && dayHasPassed -> Color.Gray
+                                isToday -> Color.White
+                                else -> Color.Black
+                            },
+                            fontWeight = if (isToday) FontWeight.Bold else FontWeight.Normal
+                        )
+                    )
+                }
+
+                if (isToday) {
+                    Column(
+                        modifier = Modifier.weight(WEIGHT_7DAY_WEEK),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(28.dp)
+                                .clip(CircleShape)
+                                .background(TurquoiseColor)
+                        ) {
+                            widget()
+                        }
+                    }
+                } else widget()
+            },
+            priorMonthDay = { dayDate ->
+                DefaultDay(
+                    text = dayDate.day.toString(),
+                    style = TextStyle(color = LightGray),
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxWidth()
+                        .weight(WEIGHT_7DAY_WEEK)
+                )
+            },
+            headerContainer = { header ->
+                Card {
+                    header()
+                }
+            },
+        )
+    )
+}
